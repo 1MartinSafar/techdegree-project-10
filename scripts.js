d79c2e1ffb3b4168cf2fcb0d865e10f0d70903ac
Martin Safar <1martinsafar@gmail.com>
2017-11-08 19:55:07 +0100

    Finished Styling 12 Random Users in a Grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
+
+
+// CAPITALIZING THE FIRST LETTER
+function capitalize(string)
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// DISPLAYING 12 RANDOM USERS
 $.ajax({
-  // url: 'https://randomuser.me/api/?results=12?nat=us',
-  // url: 'https://randomuser.me/api/?results=12',
   url: 'https://randomuser.me/api/?results=12&nat=us',
   dataType: 'json',
   success: function(data) {
@@ -13,17 +20,19 @@ $.ajax({
       // console.log(item["email"]);
       // console.log(item["location"]["city"]);
       // console.log(item["nat"]);
-
       let picture = item["picture"]["large"];
-      let first_name = item["name"]["first"];
-      let last_name = item["name"]["last"];
+      let first_name = capitalize(item["name"]["first"]);
+      let last_name = capitalize(item["name"]["last"]);
       let email = item["email"];
-      let city = item["location"]["city"];
-      // <div class="item">1</div>
+      let city = capitalize(item["location"]["city"]);
+
+      // ADDING 12 RANDOM USERS
 
       htmlString += '<div class="item">';
 
+      htmlString += '<div class="pic-div">';
       htmlString += '<img src="' + picture + '" class="picture">';
+      htmlString += '</div>';
 
       htmlString += '<div class="info_basic">';
       htmlString += '<h2 class="name">' + first_name + ' ' + last_name + '</h2>';
@@ -32,17 +41,32 @@ $.ajax({
       htmlString += '</div>';
 
       htmlString += '</div>';
-    }); // end each loop
 
+      // ADDING THE CLICKED USER TO A MODAL POP UP WINDOW
+
+    }); // end each loop
     $('.grid').html(htmlString);
 
-  // $('#videos').html(htmlString + "</ul>");
+    $('.item').on("click", function(e) {
+      console.log("CLICKED");
+      // console.log(e.target);
+    });
   }
 }); // end ajax
 
+// const item = document.querySelector(".item");
+// console.log(item);
 
+// item.addEventListener("click", function() {
+//   console.log("CLICKED");
+// });
 
-
+// const grid = document.querySelector(".grid");
+//
+// grid.addEventListener("click", function(event) {
+//   console.log("CLICKED");
+//   console.log(event.target);
+// });
 
 
 
