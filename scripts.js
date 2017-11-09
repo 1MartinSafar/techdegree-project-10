9193cdc8c65a6cefb0eacd05824efbd07f862014
Martin Safar <1martinsafar@gmail.com>
2017-11-09 10:56:58 +0100

    Modal Window Layout and Styling done (bg-color for testing)

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 
 
-// CAPITALIZING THE FIRST LETTER
+// CAPITALIZING THE FIRST LETTER IN A STRING
 function capitalize(string)
 {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// CAPITALIZING EVERY WORD IN A STRING
+function capitalizeAll(str) {
+   let splitStr = str.toLowerCase().split(' ');
+   for (let i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+   }
+   return splitStr.join(' ');
 }
 
 // DISPLAYING 12 RANDOM USERS
@@ -13,18 +22,21 @@ $.ajax({
   success: function(data) {
     console.log(data);
     let htmlString = "";
+    let modalString = "";
     $.each(data.results, function(i, item) {
-      // console.log(item["picture"]["large"]);
-      // console.log(item["name"]["first"]);
-      // console.log(item["name"]["last"]);
-      // console.log(item["email"]);
-      // console.log(item["location"]["city"]);
-      // console.log(item["nat"]);
       let picture = item["picture"]["large"];
-      let first_name = capitalize(item["name"]["first"]);
-      let last_name = capitalize(item["name"]["last"]);
+      let first_name = capitalizeAll(item["name"]["first"]);
+      let last_name = capitalizeAll(item["name"]["last"]);
       let email = item["email"];
-      let city = capitalize(item["location"]["city"]);
+      let city = capitalizeAll(item["location"]["city"]);
+
+      let username = item["login"]["username"];
+      let cell = item["cell"];
+      let street = capitalizeAll(item["location"]["street"]);
+      // let city = capitalize(item["location"]["city"]);
+      let state = capitalizeAll(item["location"]["state"]);
+      let postcode = item["location"]["postcode"];
+      let birthdate = item["dob"].slice(0,10);
 
       // ADDING 12 RANDOM USERS
 
@@ -37,42 +49,62 @@ $.ajax({
       htmlString += '<div class="info_basic">';
       htmlString += '<h2 class="name">' + first_name + ' ' + last_name + '</h2>';
       htmlString += '<span class="email">' + email + '</span>';
-      htmlString += '<span class="city">' + city + '</spain>';
+      htmlString += '<span class="city">' + city + '</span>';
       htmlString += '</div>';
 
       htmlString += '</div>';
 
-      // ADDING 12 RANDOM USERS
+      // ADDING 12 DETAILS
 
+      modalString += '<div class="details">';
 
+      modalString += '<div class="pic-div_details">';
+      modalString += '<img src="' + picture + '" class="picture">';
+      modalString += '</div>';
+
+      modalString += '<div class="info_details">';
+      modalString += '<h2 class="name">' + first_name + ' ' + last_name + '</h2>';
+      modalString += '<span class="username">' + username + '</span>';
+      modalString += '<span class="email_details">' + email + '</span>';
+      modalString += '<span class="cell">' + cell + '</span>';
+      modalString += '<span class="street">' + street + '</span>';
+      // modalString += '<span class="city">' + city + '</span>';
+      modalString += '<span class="state_postcode_city">' + city + ", " + state + " " + postcode + '</span>';
+      // modalString += '<span class="postcode">' + postcode + '</span>';
+      modalString += '<span class="birthdate">' + "Birthday: " + birthdate + '</span>';
+      modalString += '</div>';
+
+      modalString += '</div>';
     }); // end each loop
 
     // ADDING THE CLICKED USER TO A MODAL POP UP WINDOW
       // CHANGE: MAKING IT IN HTML and then ADDING TO IT
       // htmlString += '<div class="modal">';
+      // CHANGE: creating 12 divs with details, then adding them to .MODAL
 
-      const grid = document.querySelector(".grid");
-
-      grid.addEventListener("click", function(event) {
-        // console.log("CLICKED");
-        // console.log(event.target);
-
-        let modalString = "";
-
-        // can use if === .item else .parent
-        // can use z-index?
-
-        // if (event.target.className === "item") {
-        //   console.log(event.target);
-        // } else {
-        //   console.log(event.target.parentNode);
-        // }
-        console.log(event.currentTarget.firstChild);
-
-
-      });
+      // const grid = document.querySelector(".grid");
+      //
+      // grid.addEventListener("click", function(event) {
+      //   // console.log("CLICKED");
+      //   // console.log(event.target);
+      //
+      //   let modalString = "";
+      //
+      //   // can use if === .item else .parent
+      //   // can use z-index?
+      //
+      //   // if (event.target.className === "item") {
+      //   //   console.log(event.target);
+      //   // } else {
+      //   //   console.log(event.target.parentNode);
+      //   // }
+      //   console.log(event.currentTarget.firstChild);
+      //
+      //
+      // });
 
     $('.grid').html(htmlString);
+    $('.modal').html(modalString);
 
     // $('.item').on("click", function(e) {
     //   console.log("CLICKED");
