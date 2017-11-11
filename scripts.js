9b8d0eb7ba0ed6a81a16f7387944ca9832b96b5f
Martin Safar <1martinsafar@gmail.com>
2017-11-11 09:29:17 +0100

    HTML, CSS, JavaScript files cleaned

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 
 
 // CAPITALIZING THE FIRST LETTER IN A STRING
-function capitalize(string)
-{
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // CAPITALIZING EVERY WORD IN A STRING
@@ -65,71 +64,23 @@ $.ajax({
       modalString += '<span class="email_details">' + email + '</span>';
       modalString += '<span class="cell">' + cell + '</span>';
       modalString += '<span class="street">' + street + '</span>';
-      // modalString += '<span class="city">' + city + '</span>';
       modalString += '<span class="state_postcode_city">' + city + ", " + state + " " + postcode + '</span>';
-      // modalString += '<span class="postcode">' + postcode + '</span>';
       modalString += '<span class="birthdate">' + "Birthday: " + birthdate + '</span>';
       modalString += '</div>';
 
       modalString += '</div>';
     }); // end each loop
 
-    // ADDING THE CLICKED USER TO A MODAL POP UP WINDOW
-      // CHANGE: MAKING IT IN HTML and then ADDING TO IT
-      // htmlString += '<div class="modal">';
-      // CHANGE: creating 12 divs with details, then adding them to .MODAL
-
-      // const grid = document.querySelector(".grid");
-      //
-      // grid.addEventListener("click", function(event) {
-      //   // console.log("CLICKED");
-      //   // console.log(event.target);
-      //
-      //   let modalString = "";
-      //
-      //   // can use if === .item else .parent
-      //   // can use z-index?
-      //
-      //   // if (event.target.className === "item") {
-      //   //   console.log(event.target);
-      //   // } else {
-      //   //   console.log(event.target.parentNode);
-      //   // }
-      //   console.log(event.currentTarget.firstChild);
-      //
-      //
-      // });
-
     $('.grid').html(htmlString);
-    // $('.modal').html(modalString);
     $('.modal').append(modalString);
-
-    // $('.item').on("click", function(e) {
-    //   console.log("CLICKED");
-      // console.log(e.target);
-    // });
-  }
+  } // end callback (success)
 }); // end ajax
-
-// const item = document.querySelector(".item");
-// console.log(item);
-
-// item.addEventListener("click", function() {
-//   console.log("CLICKED");
-// });
-
-// const grid = document.querySelector(".grid");
-//
-// grid.addEventListener("click", function(event) {
-//   console.log("CLICKED");
-//   console.log(event.target);
-// });
 
 const modal = document.querySelector(".modal");
 const modal_close = document.querySelector(".modal-close");
 const grid = document.querySelector(".grid");
-// let items = document.querySelectorAll(".item");
 
+// Hiding the modal window and all the details when the modal is closed
 modal_close.addEventListener("click", function() {
   let details = document.querySelectorAll(".details");
   modal.style.display = "none";
@@ -138,10 +89,6 @@ modal_close.addEventListener("click", function() {
     details[i].style.display = "none";
   }
 });
-
-//     var array = [2, 9, 9];
-//     array.indexOf(2);     // 0
-//     array.indexOf(7);     // -1
 
 grid.addEventListener("click", function(event) {
   let clicked = event.target;
@@ -153,13 +100,10 @@ grid.addEventListener("click", function(event) {
 
     if (clicked.className === "item") {
       user = event.target;
-      // console.log(user);
     } else if ((clicked.className === "pic-div") || (clicked.className === "info_basic")) {
       user = event.target.parentNode;
-      // console.log(user);
     } else {
       user = event.target.parentNode.parentNode;
-      // console.log(user);
     }
     // RESETTING all details to be hidden - in case the user
     // clicks on another employee while the modal window is still open
@@ -173,11 +117,8 @@ grid.addEventListener("click", function(event) {
     let nextDetails = ""
     for (let i = 0; i < items.length; i++) {
       if (items[i] === user) {
-        // console.log("ITEM FOUND: " + items[i].innerHTML);
         details[i].style.visibility = "visible";
         details[i].style.display = "flex";
-        // console.log("DETAIL FOUND: " + details[i].innerHTML);
-
         currentDetails = details[i];
       }
     }
@@ -189,13 +130,8 @@ grid.addEventListener("click", function(event) {
       details[i].style.visibility = "hidden";
       details[i].style.display = "none";
     }
-
-      // let previousDetails = "";
-      // let nextDetails = "";
       currentDetails.style.visibility = "hidden";
       currentDetails.style.display = "none";
-      // console.log("CURRENT DETAILS:");
-      // console.log(currentDetails);
       console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
       console.log("PREV CLICKED, CURRENTLY: ");
       console.log("currentDetails: " + currentDetails.textContent);
@@ -204,56 +140,23 @@ grid.addEventListener("click", function(event) {
 
       if (currentDetails.previousElementSibling.className !== "details") {
         console.log(">>>>> AT THE BEGINNING <<<<<");
-        // currentDetails.nextElementSibling.style.visibility = "visible";
-        // currentDetails.nextElementSibling.style.display = "flex";
-
-        // Node.parentNode.lastChild
-
-        // console.log(currentDetails.parentNode.lastElementChild.innerHTML);
         currentDetails = currentDetails.parentNode.lastElementChild;
-
-        // // TESTING
-        // while (currentDetails.className !== "details") {
-        //   currentDetails = currentDetails.previousElementSibling;
-        //   // console.log("NEW: " + currentDetails.tagName);
-        // }
-
         previousDetails = currentDetails;
-
         currentDetails.style.visibility = "visible";
         currentDetails.style.display = "flex";
         console.log("SHOWING: " + currentDetails.textContent);
-
-        // SOMETHING WRONG WITH THE PREVIOUS EMPLOYEE NOT RESETTING,
-        // KEEPS GOING BACK FROM THE LAST ITEM THAT WAS USED AS BACK
-
-
       }
       else {
-        // console.log("CURRENT DETAILS:");
-        // console.log(currentDetails);
-
         previousDetails = currentDetails.previousElementSibling;
-        // console.log("PREVIOUS DETAILS:");
-        // console.log(previousDetails);
-
-        // console.log(previousDetails.innerHTML);
         previousDetails.style.visibility = "visible";
         previousDetails.style.display = "flex";
         currentDetails = previousDetails;
-
-        // console.log("PREVIOUS DETAILS:");
-        // console.log(previousDetails);
       }
-
       console.log("NEWLY: ");
       console.log("currentDetails: " + currentDetails.textContent);
       console.log("previousDetails: " + previousDetails.textContent);
       console.log("nextDetails: " + nextDetails.textContent);
-
-
-
-    });
+    }); // end prev
 
     // MOVE FORTH BETWEEN EMPLOYEE DETAILS INFO
     const next = document.querySelector(".modal-next");
@@ -266,8 +169,6 @@ grid.addEventListener("click", function(event) {
 
       currentDetails.style.visibility = "hidden";
       currentDetails.style.display = "none";
-      // console.log("CURRENT DETAILS:");
-      // console.log(currentDetails);
       console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
       console.log("NEXT CLICKED, CURRENTLY: ");
       console.log("currentDetails: " + currentDetails.textContent);
@@ -277,98 +178,39 @@ grid.addEventListener("click", function(event) {
       if (!(currentDetails.nextElementSibling) ||
           currentDetails.nextElementSibling.className !== "details") {
         console.log(">>>>> AT THE END <<<<<");
-        // currentDetails.nextElementSibling.style.visibility = "visible";
-        // currentDetails.nextElementSibling.style.display = "flex";
-
-        // console.log("CURRENT DETAILS: " + currentDetails.innerHTML);
-        // console.log("CURRENT DETAILS: " + currentDetails.parentNode.firstElementChild.tagName);
-        // console.log(currentDetails.previousElementSibling.parentNode.firstChild.innerHTML);
-        // currentDetails = nextDetails.previousElementSibling.parentNode.lastChild;
         currentDetails = currentDetails.parentNode.firstElementChild;
-
-        // console.log("CURRENT DETAILS TAG: " + currentDetails.tagName);
         while (currentDetails.className !== "details") {
           currentDetails = currentDetails.nextElementSibling;
-          // console.log("NEW: " + currentDetails.tagName);
         }
-
         nextDetails = currentDetails;
         currentDetails.style.visibility = "visible";
         currentDetails.style.display = "flex";
         console.log("SHOWING: " + currentDetails.textContent);
       }
       else {
-        // console.log("CURRENT DETAILS:");
-        // console.log(currentDetails);
-
         nextDetails = currentDetails.nextElementSibling;
-        // console.log("PREVIOUS DETAILS:");
-        // console.log(nextDetails);
-
-        // console.log(nextDetails.innerHTML);
         nextDetails.style.visibility = "visible";
         nextDetails.style.display = "flex";
         currentDetails = nextDetails;
-
-        // console.log("PREVIOUS DETAILS:");
-        // console.log(nextDetails);
       }
-      // Node.parentNode.lastChild
       console.log("NEWLY: ");
       console.log("currentDetails: " + currentDetails.textContent);
       console.log("previousDetails: " + previousDetails.textContent);
       console.log("nextDetails: " + nextDetails.textContent);
-
-
-    });
-
-
-
-
+    }); // end next
   }
-
-  // let item = event.currentTarget.;
-  // if (clicked.className !== "grid") {
-    // modal.style.display = "block";
-
-    // const items = document.querySelectorAll(".item")
-    // const details = document.querySelectorAll(".details")
-    // console.log(items);
-    // console.log(details);
-
-
-    // var array = [2, 9, 9];
-    // array.indexOf(2);     // 0
-    // array.indexOf(7);     // -1
-  // }
-});
-
-// function display(event) {
-//   // let item = event.currentTarget;
-//   console.log(modal.style.display);
-//   modal.style.display = "block";
-//   console.log(modal.style.display);
-// };
-//
-// for (let i = 0; i < items.length; i++) {
-//     items[i].addEventListener('click', display);
-// }
-
-
+}); // end grid events
 
 // SEARCH function
-
 function search() {
 
   let input = document.querySelector("#searchBar");
   let filter = input.value.toUpperCase();
-
   let grid = document.querySelector(".grid");
   let items = document.querySelectorAll(".item");
 
   for (let i = 0; i < items.length; i++) {
     let item = items[i];
-    // console.log(item.lastElementChild.firstElementChild.textContent);
     let name = item.lastElementChild.firstElementChild.textContent;
     if (name.toUpperCase().indexOf(filter) > -1) {
       items[i].style.display = "";
@@ -377,12 +219,6 @@ function search() {
     }
   }
 }
-
-
-
-
-
-
 
 
 
